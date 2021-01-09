a66919fa9351b089124216bc6aa596538dea5cc2
Lucas Gallon <lucas-gallon@hotmail.com>
2021-01-09 09:22:20 -0300

    Inicial Commit

---
+++ javascript_note_api/app/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+let userSchema = new mongoose.Schema({
+    name: String,
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    createdAt: { type: Date, default: Date.now},
+    updated_at: { type: Date, default: Date.now}
+});
+
+userSchema.pre('save', function (next){
+    if(this.isNew || this.isModified('password')){
+        bcrypt.hash(this.password, 10,
+            (err, hashedPassword) => {
+            if (err){
+                next(err)
+            }
+                else{
+                    this.password = hashedPassword;
+                    next();
+                }
+            })
+        }
+});
+
+module.exports = mongoose.model('User',userSchema);
